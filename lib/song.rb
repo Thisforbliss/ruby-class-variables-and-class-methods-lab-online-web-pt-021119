b2cb821ea7d5f36b4667abec5366b129ab5c1d54
Malcolm Leake <echelon1089@gmail.com>
2019-03-14 20:56:31 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+require 'pry'
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@genres << @genre 
+  @@artists << @artist
+  @@count += 1 
+  end
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.genres 
+    @@genres.uniq
+  end 
+  
+  def self.artists
+    @@artists.uniq
+  end
+    
+  
+  def self.genre_count
+   genres = {}
+   
+    @@genres.each do |genre|
+      if genres[genre] 
+        #binding.pry
+       genres[genre] += 1 
+      else
+        genres[genre] = 1
+   binding.pry
+      end 
+  genre
+    end
+  end
+  
+  
+end 
